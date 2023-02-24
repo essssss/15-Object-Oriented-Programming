6c898061abe0cfab6de9483d63a2cbdf969deb5c
essssss <srmcnamara7@gmail.com>
2023-02-24 11:41:06 -0500

    Part One

---
+++ exercise.js
@@ -0,0 +1,13 @@
+class Vehicle {
+   constructor(a, b, c) {
+      this.make = a;
+      this.model = b;
+      this.year = c;
+   }
+   honk() {
+      return 'Beep.';
+   }
+   toString() {
+      return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+   }
+}
